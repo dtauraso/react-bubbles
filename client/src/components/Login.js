160754c851da7863dfa148190144e768d91936de
David Tauraso <david318@me.com>
2019-12-20 08:55:05 -0800

    The bubbles display

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,75 +3,81 @@ import React, {useState} from "react";
 import { axiosWithAuth } from '../utils/axiosWithAuth'
 import Loader from 'react-loader-spinner'
 
-const Login = (props) => {
+class Login extends React.Component {
+
+  state = {
+    credentials: {
+        username: '',
+        password: ''
+    },
+    isFetching: false
+}
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
-  const [state, setState] = useState({
-    credentials: {
-      username: '',
-      password: ''
-    },
-    isFetching: false
-  })
+  
 
-  const handleChange = e => {
+  handleChange = e => {
 
-    setState({
+    this.setState({
       credentials: {
-        ...state.credentials,
+        ...this.state.credentials,
         [e.target.name]: e.target.value
       }
     })
+    console.log(this.state)
   }
-  const login = e => {
+  login = e => {
       e.preventDefault();
-      setState({
+      this.setState({
         isFetching: true
       })
       axiosWithAuth()
-        .post('/login', state.credentials)
+        .post('/login', this.state.credentials)
         .then(res => {
           localStorage.setItem('token', res.data.payload)
-          props.history.push()
+          this.props.history.push('/bubblePage')
         })
         .catch(err =>  console.log(err))
 
     }
 
-  return (
-    <div>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-      <form onSubmit={login}>
-          <input
-              type="text"
-              name="username"
-              value={state.credentials.username}
-              onChange={handleChange}
-
-          />
-          <input
-              type="password"
-              name="password"
-              value={state.credentials.password}
-              onChange={handleChange}
-
-          />
-          <button>Log in</button>
-          {state.isFetching && 
-              <Loader
-              type="Puff"
-              color="#00BFFF"
-              height={100}
-              width={100}
-              //  timeout={3000} //3 secs
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the Bubble App!</h1>
+        <p>Build a login page here</p>
+        <form onSubmit={this.login}>
+            <input
+                type="text"
+                name="username"
+                value={this.state.credentials.username}
+                onChange={this.handleChange}
   
-          /> 
-          }
-      </form>
-    </div>
-  );
+            />
+            <input
+                type="password"
+                name="password"
+                value={this.state.credentials.password}
+                onChange={this.handleChange}
+  
+            />
+            <button>Log in</button>
+            {this.state.isFetching && 
+                <Loader
+                type="Puff"
+                color="#00BFFF"
+                height={100}
+                width={100}
+                //  timeout={3000} //3 secs
+    
+            /> 
+            }
+        </form>
+      </div>
+    );
+  }
+  
 };
 
 export default Login;
