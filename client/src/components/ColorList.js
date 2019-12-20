20616d96f2b27f0e599a74d5f391745480f6665d
David Tauraso <david318@me.com>
2019-12-20 09:35:56 -0800

    Can delete a color

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -23,10 +23,7 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
-    // updateColors(colors.map(color => {
-    //   return color.code.hex === colorCode ? 
-      
-    // }))
+
     axiosWithAuth()
       .put(`http://localhost:5000/api/colors/${color.id}`, color)
       .then(res => {
@@ -43,6 +40,16 @@ const ColorList = ({ colors, updateColors }) => {
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${color.id}`)
+      .then(res => {
+        // console.log(res.data)
+        updateColors(colors.filter(color => color.id !== res.data))
+
+      })
+      .catch(err => {
+        console.log(err)
+      })
   };
 
   return (
